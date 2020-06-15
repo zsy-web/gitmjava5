a07fa24fcc7e0fd21b6a0958d973f05e4b399abd
zsy-web <1185544922@qq.com>
2020-06-16 00:17:59 +0800

    第一次提交

---
+++ src/main/java/com/hr/gitmjava5/Gitmjava5Application.java
@@ -0,0 +1,13 @@
+package com.hr.gitmjava5;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Gitmjava5Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Gitmjava5Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/hr/gitmjava5/Gitmjava5ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hr.gitmjava5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Gitmjava5ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
